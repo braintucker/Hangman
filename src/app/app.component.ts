eded4ac35f3ee78e2b2c0b7ed393ea9f7946454f
Brain Tucker <brian.briantucker@gmail.com>
2017-10-17 13:19:44 -0500

    created the letter-buttons.component to display letters. They disable themselves when clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <h1>Guess The Word</h1>
     <p>{{solution | mask:selection}}</p>
+    <letter-buttons></letter-buttons>
   `
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MaskPipe } from './mask.pipe';
+import { LetterButtonsComponent } from './letter-buttons.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, MaskPipe],
+  declarations: [AppComponent, MaskPipe, LetterButtonsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
