38ee9e2c572071e651f4cb28ef9ec8e65d67dff3
Brain Tucker <brian.briantucker@gmail.com>
2017-10-17 13:55:44 -0500

    using TestBed to test the angular component, LetterButtonsComponent

--- src/app/letter-buttons.component.spec.ts
+++ src/app/letter-buttons.component.spec.ts
@@ -1,12 +1,19 @@
+import { TestBed } from '@angular/core/testing';
 import { LetterButtonsComponent} from './letter-buttons.component';
 
 describe('LetterButtonsComponent', () => {
 
-  const component = new LetterButtonsComponent();
 
-  it('should display a button for each letter of the alphabet', () => {
-    expect(component.alphabet).toEqual(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LetterButtonsComponent]
+    });
+  });
+
+  fit('should display a button for each letter of the alphabet', () => {
+    const fixture = TestBed.createComponent(LetterButtonsComponent);
+    const component = fixture.componentInstance;
+    console.log('component', component);
   });
 
 });
